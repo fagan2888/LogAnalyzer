0b2b28fb7fc91f39183dcb5173488ca5502f850b
sjinib <sjin@interactivebrokers.com>
2016-07-29 15:34:38 -0500

    7/29/2016

---
+++ Temp/ssoOut.t
@@ -0,0 +1,1 @@
+sso|IS: 07:51:15:729 &action=RISK_PRSCAN&hardware_info=35f32e94|34:17:EB:B4:8C:62&time_out=10000 ms
---
+++ src/com/ib/parser/Choices.java
@@ -0,0 +1,6 @@
+package com.ib.parser;
+
+public class Choices {
+	public final static int API = 7;
+	public final static int MD = 6;
+}
